bd7303c6018c75502eb0839955cf12fa2536c825
Iulian Oana <iulian.andreioana@gmail.com>
2020-03-31 17:52:12 +0100

    9. Animation builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/animation.dart';
 
 void main() {
   runApp(new MaterialApp(
@@ -12,29 +11,33 @@ class MyApp extends StatefulWidget {
   _State createState() => new _State();
 }
 
-class _State extends State<MyApp> with SingleTickerProviderStateMixin {
+class _State extends State<MyApp> with TickerProviderStateMixin {
 
-  Animation<double> animation;
+  Animation animation;
   AnimationController controller;
+
 
   @override
   void initState() {
     super.initState();
 
-    controller = new AnimationController(duration: const Duration(milliseconds: 5000), vsync: this);
-    animation = new Tween(begin: 0.0, end: 200.0).animate(controller);
-    animation.addListener(() {
-      setState(() {
-        // The state of the animation has changed.
-      });
-    });
+    controller = new AnimationController(
+      duration: const Duration(seconds: 10),
+      vsync: this);
+    final CurvedAnimation curve = new CurvedAnimation(
+      parent: controller,
+      curve: Curves.bounceIn);
+    animation = new Tween(begin: 0.0, end: 300.0).animate(curve);
+
     controller.forward();
   }
 
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
+  Widget builder(BuildContext context, Widget child) {
+    return new Container(
+      height: animation.value,
+      width: animation.value,
+      child: new FlutterLogo(),
+    );
   }
 
   @override
@@ -45,10 +48,8 @@ class _State extends State<MyApp> with SingleTickerProviderStateMixin {
       ),
       body: new Container(
         padding: new EdgeInsets.all(32.0),
-        height: animation.value,
-        width: animation.value,
         child: new Center(
-          child: new FlutterLogo(size: 300.0),
+          child: new AnimatedBuilder(animation: animation, builder: builder)
         ),
       ),
     );

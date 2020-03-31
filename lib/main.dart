7487d697ea552da128a1ba8d255b1d48b1795a1f
Iulian Oana <iulian.andreioana@gmail.com>
2020-03-31 18:46:36 +0100

    10. Listener

--- lib/main.dart
+++ lib/main.dart
@@ -22,21 +22,31 @@ class _State extends State<MyApp> with TickerProviderStateMixin {
     super.initState();
 
     controller = new AnimationController(
-      duration: const Duration(seconds: 10),
-      vsync: this);
+      duration: const Duration(milliseconds: 2000),
+      vsync: this
+    );
+
     final CurvedAnimation curve = new CurvedAnimation(
       parent: controller,
-      curve: Curves.bounceIn);
-    animation = new Tween(begin: 0.0, end: 300.0).animate(curve);
-
+      curve: Curves.easeIn);
+    animation = new Tween(begin: 100.0, end: 300.0).animate(curve);
+    animation.addStatusListener(listener);
     controller.forward();
+  }
+
+  void listener(AnimationStatus status) {
+    if(status == AnimationStatus.completed) {
+      controller.reverse();
+    }else if(status == AnimationStatus.dismissed) {
+      controller.forward();
+    }
   }
 
   Widget builder(BuildContext context, Widget child) {
     return new Container(
       height: animation.value,
       width: animation.value,
-      child: new FlutterLogo(),
+      child: new FlutterLogo()
     );
   }
 

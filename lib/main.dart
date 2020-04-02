ca51c3707e76d6e55822d8fe8193e78698eefc7e
Iulian Oana <iulian.andreioana@gmail.com>
2020-04-02 12:25:28 +0100

    29. Firebase Real Time Database

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:io';
 import 'auth.dart' as fbAuth;
 import 'storage.dart' as fbStorage;
+import 'database.dart' as fbDatabase;
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:path/path.dart'; //needed for basename
 
 void main() async {
@@ -17,7 +20,8 @@ void main() async {
           googleAppID: '1:91956350870:android:3fdefccd00b63954e757b7', // mobilesdk_app_id
           gcmSenderID: '91956350870', // project_number
           apiKey: 'AIzaSyDsmp6LnFU32P-y2NrhbXw5DQJyNqss2Eg', // current_key
-          projectID: 'fir-app-eb02c' // project_id
+          projectID: 'fir-app-eb02c', // project_id
+          databaseURL: 'https://fir-app-eb02c.firebaseio.com/', // firebase_url
       )
   );
 
@@ -27,28 +31,39 @@ void main() async {
     storageBucket: 'gs://fir-app-eb02c.appspot.com' // Firebase > Storage
   );
 
+  final FirebaseDatabase database = new FirebaseDatabase(
+    app: app
+  );
+
   runApp(new MaterialApp(
-    home: new MyApp(storage: storage,),
+    home: new MyApp(app: app, database: database, storage: storage,),
   ),);
 }
 
 class MyApp extends StatefulWidget {
-  MyApp({this.storage});
+  MyApp({this.app, this.database, this.storage});
   final FirebaseStorage storage;
+  final FirebaseApp app;
+  final FirebaseDatabase database;
 
   @override
-  _State createState() => new _State(storage: storage);
+  _State createState() => new _State(app: app, database: database, storage: storage);
 }
 
 class _State extends State<MyApp> {
-  _State({this.storage});
+  _State({this.app, this.database, this.storage});
   final FirebaseStorage storage;
+  final FirebaseApp app;
+  final FirebaseDatabase database;
 
   String _status;
   String _location;
+  StreamSubscription<Event> _counterSubscription;
 
   @override
   void initState() {
+    super.initState();
+    fbDatabase.init(database);
     _status = 'Not Authenticated';
     _signIn();
   }
@@ -70,6 +85,7 @@ class _State extends State<MyApp> {
       setState(() {
         _status = 'Signed in';
       });
+      _initDatabase(database);
     }else{
       setState(() {
         _status = 'Could not sign in';
@@ -105,6 +121,33 @@ class _State extends State<MyApp> {
     });
   }
 
+  void _initDatabase(FirebaseDatabase database) async {
+    await fbDatabase.init(database);
+
+    _counterSubscription = fbDatabase.counterRef.onValue.listen((event) {
+      setState(() {
+        fbDatabase.error = null;
+        fbDatabase.counter = event.snapshot.value ?? 0;
+      });
+    }, onError: (Object o) {
+      final DatabaseError error = o;
+      setState(() {
+        fbDatabase.error = error;
+      });
+      print('Error initiating the Database: $error');
+    });
+  }
+
+  void _increment() async {
+    int value = fbDatabase.counter + 1;
+    fbDatabase.setCounter(value);
+  }
+
+  void _decrement() async {
+    int value = fbDatabase.counter - 1;
+    fbDatabase.setCounter(value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -117,6 +160,8 @@ class _State extends State<MyApp> {
           child: new Column(
             children: <Widget>[
               new Text(_status),
+              new Text('Counter ${fbDatabase.counter}'),
+              new Text('Error: ${fbDatabase.error}'),
               new RaisedButton(
                 onPressed: _signOut,
                 child: new Text('Sign out'),
@@ -142,6 +187,20 @@ class _State extends State<MyApp> {
                   new RaisedButton(
                     onPressed: _download,
                     child: new Text('Download'),
+                  ),
+                ],
+              ),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  new RaisedButton(
+                    onPressed: _increment,
+                    child: new Text('Increment'),
+                  ),
+                  new RaisedButton(
+                    onPressed: _decrement,
+                    child: new Text('Decrement'),
                   ),
                 ],
               ),
